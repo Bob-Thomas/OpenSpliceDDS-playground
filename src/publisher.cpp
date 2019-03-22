7293610c8a42d2c4964f5cde62931a091790d962
Bob Thomas <bobthomas295@gmail.com>
2019-03-22 10:53:45 +0100

    Add comments to the code

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 #include <gen/HelloWorldData_DCPS.hpp>
-#include <thread>         // std::thread, std::this_thread::sleep_for
-#include <chrono> 
+#include <thread> // std::thread, std::this_thread::sleep_for
+#include <chrono>
 #include <sstream>
 #include <string>
 
-
-std::ostream& operator<< (std::ostream& os, const HelloWorldData::Msg& ts) {
+/**
+ * @brief Simple overload to print out pretty HelloWorld structure
+ *
+ * @param os
+ * @param ts
+ * @return std::ostream&
+ */
+std::ostream &operator<<(std::ostream &os, const HelloWorldData::Msg &ts)
+{
   os << ts.message();
   return os;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+
+  // Setup the domain participant and create the helloworld topic
   dds::domain::DomainParticipant dp(0);
   dds::topic::Topic<HelloWorldData::Msg> topic(dp, "HelloWorld");
+
+  // Create a subscriber for the helloWorld topic and attach a DataWriter
   dds::pub::Publisher pub(dp);
   dds::pub::DataWriter<HelloWorldData::Msg> dw(pub, topic);
-    
 
-  // Initialize random number generation with a seed
+  // Enable random number generator
+  srand(time(NULL));
+
   char tab[200];
+
+  // Create base message with random ID
   HelloWorldData::Msg msg;
-  msg.userID(1);
+  msg.userID(rand());
   msg.message(DDS::string_dup(""));
   std::cout << "write things\n";
 
-  while(true) {
-    std::cin.getline(tab,200);
+  while (true)
+  {
+    // Wait for console input and then transmit it over the DDS
+    std::cin.getline(tab, 200);
     msg.message(tab);
     dw.write(msg);
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -3,10 +3,20 @@
 #include <thread> // std::thread, std::this_thread::sleep_for
 #include <chrono>
 
-template<typename T>
+/**
+ * @brief Generic listener for DDS data readers
+ *
+ * @tparam T
+ */
+template <typename T>
 class ModelListener : public dds::sub::NoOpDataReaderListener<T>
 {
   public:
+    /**
+     * @brief Callback that the dataReader calls when data is available
+     *
+     * @param dr data reader
+     */
     virtual void on_data_available(
         dds::sub::DataReader<T> &dr)
     {
@@ -21,13 +31,19 @@ class ModelListener : public dds::sub::NoOpDataReaderListener<T>
 int main(int argc, char *argv[])
 {
 
+    // Setup the domain participant and create the helloworld topic
     dds::domain::DomainParticipant dp(0);
     dds::topic::Topic<HelloWorldData::Msg> topic(dp, "HelloWorld");
+
+    // Create a subscriber for the helloWorld topic and attach a DataReader
     dds::sub::Subscriber sub(dp);
     dds::sub::DataReader<HelloWorldData::Msg> dr(sub, topic);
 
+    // Use the listener pattern to listen for messsages
     ModelListener<HelloWorldData::Msg> listener;
     dr.listener(&listener, dds::core::status::StatusMask::data_available());
+
+    // keep the program alive
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
