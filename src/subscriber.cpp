6b9e2a6fb1688ccf3d8748fa17399af9adb3027e
Bob Thomas <bobthomas295@gmail.com>
2019-03-28 18:51:39 +0100

    SUBSCRIBER: Implement camera DDS controllre into the publisher

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,51 +1,31 @@
 #include <iostream>
-#include <gen/HelloWorldData_DCPS.hpp>
 #include <thread> // std::thread, std::this_thread::sleep_for
+#include <string>
 #include <chrono>
+#include <src/cameraDDS.hpp>
 
-/**
- * @brief Generic listener for DDS data readers
- *
- * @tparam T
- */
 template <typename T>
 class ModelListener : public dds::sub::NoOpDataReaderListener<T>
 {
   public:
-    /**
-     * @brief Callback that the dataReader calls when data is available
-     *
-     * @param dr data reader
-     */
     virtual void on_data_available(
         dds::sub::DataReader<T> &dr)
     {
         auto samples = dr.read();
         std::for_each(samples.begin(), samples.end(),
                       [](const dds::sub::Sample<T> &s) {
-                          std::cout << s.data().message() << std::endl;
+                          std::cout << s.data() << std::endl;
                       });
     }
 };
 
 int main(int argc, char *argv[])
 {
+    CameraDDS cameraDDS(argv[1], argv[2], argv[3]);
 
-    // Setup the domain participant and create the helloworld topic
-    dds::domain::DomainParticipant dp(0);
-    dds::topic::Topic<HelloWorldData::Msg> topic(dp, "HelloWorld");
-
-    // Create a subscriber for the helloWorld topic and attach a DataReader
-    dds::sub::Subscriber sub(dp);
-    dds::sub::DataReader<HelloWorldData::Msg> dr(sub, topic);
-
-    // Use the listener pattern to listen for messsages
-    ModelListener<HelloWorldData::Msg> listener;
-    dr.listener(&listener, dds::core::status::StatusMask::data_available());
-
-    // keep the program alive
     while (true)
     {
+        cameraDDS.update();
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
     return 0;
