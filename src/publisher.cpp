1e73acff941532781d4746199b370868940f898f
Bob Thomas <bobthomas295@gmail.com>
2019-03-28 18:51:22 +0100

    PUBLISHER: Implement joystick DDS controller into the publisher

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
-#include <gen/HelloWorldData_DCPS.hpp>
+#include <gen/JoystickData_DCPS.hpp>
 #include <thread> // std::thread, std::this_thread::sleep_for
 #include <chrono>
 #include <sstream>
 #include <string>
-
-/**
- * @brief Simple overload to print out pretty HelloWorld structure
- *
- * @param os
- * @param ts
- * @return std::ostream&
- */
-std::ostream &operator<<(std::ostream &os, const HelloWorldData::Msg &ts)
-{
-  os << ts.message();
-  return os;
-}
+#include "joystick.hh"
+#include <src/inputLister.hpp>
+#include <src/joystickDDS.hpp>
 
 int main(int argc, char *argv[])
 {
 
-  // Setup the domain participant and create the helloworld topic
-  dds::domain::DomainParticipant dp(0);
-  dds::topic::Topic<HelloWorldData::Msg> topic(dp, "HelloWorld");
-
-  // Create a subscriber for the helloWorld topic and attach a DataWriter
-  dds::pub::Publisher pub(dp);
-  dds::pub::DataWriter<HelloWorldData::Msg> dw(pub, topic);
-
-  // Enable random number generator
   srand(time(NULL));
+  InputLister lister("/sys/class/input/", "js+");
+  std::cout << "Select your input device\n";
+  lister.showList(std::cout);
 
-  char tab[200];
+  int answer = 0;
+  std::cin >> answer;
+  Joystick joystick(answer);
 
-  // Create base message with random ID
-  HelloWorldData::Msg msg;
-  msg.userID(rand());
-  msg.message(DDS::string_dup(""));
-  std::cout << "write things\n";
+  if (!joystick.isFound())
+  {
+    printf("open failed.\n");
+    exit(1);
+  }
+
+  DDSJoystick joystickController(joystick);
 
   while (true)
   {
-    // Wait for console input and then transmit it over the DDS
-    std::cin.getline(tab, 200);
-    msg.message(tab);
-    dw.write(msg);
+    joystickController.update();
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
   return 0;
