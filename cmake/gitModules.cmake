cbe605297f825d31c5bedb3c65aa80c917d69ba5
Bob Thomas <bobthomas295@gmail.com>
2019-03-28 18:44:55 +0100

    CMAKE: Add cpr library for web requests

--- cmake/gitModules.cmake
+++ cmake/gitModules.cmake
@@ -1,11 +1,30 @@
+find_package(Git QUIET)
+SET(FETCHCONTENT_QUIET FALSE)
 FetchContent_Declare(
   joystick
   GIT_REPOSITORY https://github.com/drewnoakes/joystick.git
   GIT_TAG        master
 )
 
+FetchContent_Declare(
+  cpr
+  GIT_REPOSITORY https://github.com/whoshuu/cpr.git
+  GIT_TAG        master
+  GIT_PROGRESS   TRUE
+)  
+
+
 FetchContent_GetProperties(joystick)
 if(NOT joystick_POPULATED)
   FetchContent_Populate(joystick)
   include_directories(${joystick_SOURCE_DIR})
+endif()
+
+FetchContent_GetProperties(cpr)
+if(NOT cpr_POPULATED)
+  FetchContent_Populate(
+    cpr
+  )
+  add_subdirectory(${cpr_SOURCE_DIR})
+  include_directories(${CPR_INCLUDE_DIRS})
 endif()
