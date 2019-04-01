71dd0d252ea5eb69186b03efef42a083e67d89e3
Bob Thomas <bobthomas295@gmail.com>
2019-04-01 18:37:51 +0200

    ONVIF: Implement new onvif classes into subscriber

--- src/cameraDDS.hpp
+++ src/cameraDDS.hpp
@@ -1,48 +1,44 @@
 #pragma once
 #include <string>
+#include <functional>
 #include <gen/JoystickData_DCPS.hpp>
-#include <cpr/cpr.h>
-
+#include <src/onvif/OnvifPTZ.hpp>
 class CameraDDS
 {
-  private:
-    std::string cameraIP;
-    std::string username;
-    std::string password;
-    dds::domain::DomainParticipant dp;
-    dds::topic::Topic<JoystickData::Movement> topic;
-    dds::sub::Subscriber sub;
-    dds::sub::DataReader<JoystickData::Movement> dr;
-    // Create the WaitSet
-    dds::core::cond::WaitSet ws;
-    // Create a ReadCondition for our DataReader and configure it for new data
-    dds::sub::cond::ReadCondition rc;
-    void moveCamera(const JoystickData::Movement &movement)
-    {
-        std::cout << "TILT : " << movement.tiltSpeed() << "\n";
-        auto r = cpr::Get(cpr::Url{this->cameraIP + "/axis-cgi/com/ptz.cgi"},
-                          cpr::Digest{this->username, this->password},
-                          cpr::Parameters{
-                              {"continuouspantiltmove", std::to_string(movement.panSpeed()) + "," + std::to_string(movement.tiltSpeed())},
-                              {"zoom", std::to_string(movement.zoomSpeed())}});
-    }
+private:
+  dds::domain::DomainParticipant dp;
+  dds::topic::Topic<JoystickData::Movement> topic;
+  dds::sub::Subscriber sub;
+  dds::sub::DataReader<JoystickData::Movement> dr;
+  // Create the WaitSet
+  dds::core::cond::WaitSet ws;
+  // Create a ReadCondition for our DataReader and configure it for new data
+  dds::sub::cond::ReadCondition rc;
 
-  public:
-    CameraDDS(std::string cameraIP, std::string username, std::string password) : cameraIP(cameraIP), username(username), password(password), dp(dds::domain::DomainParticipant(0)), topic(dds::topic::Topic<JoystickData::Movement>(dp, "joystick")),
-                                                                                  sub(dds::sub::Subscriber(dp)), dr(dds::sub::DataReader<JoystickData::Movement>(sub, topic)), rc(dds::sub::cond::ReadCondition(dr, dds::sub::status::DataState::new_data()))
-    {
-        this->ws += this->rc;
-    }
-    void update()
-    {
-        // Wait for new data to be available
-        ws.wait();
-        // Read the data
-        auto samples = dr.read();
-        std::for_each(samples.begin(),
-                      samples.end(),
-                      [this](const dds::sub::Sample<JoystickData::Movement> &m) {
-                          this->moveCamera(m.data());
-                      });
-    }
+  std::function<void(float, float, float)> updateCameraCallback;
+
+public:
+  CameraDDS() : dp(dds::domain::DomainParticipant(0)), topic(dds::topic::Topic<JoystickData::Movement>(dp, "joystick")),
+                sub(dds::sub::Subscriber(dp)), dr(dds::sub::DataReader<JoystickData::Movement>(sub, topic)), rc(dds::sub::cond::ReadCondition(dr, dds::sub::status::DataState::new_data()))
+  {
+    this->ws += this->rc;
+  }
+
+  void setCameraUpdateCallback(std::function<void(float, float, float)> f)
+  {
+    updateCameraCallback = f;
+  }
+  
+  void update()
+  {
+    // Wait for new data to be available
+    ws.wait();
+    // Read the data
+    auto samples = dr.read();
+    std::for_each(samples.begin(),
+                  samples.end(),
+                  [this](const dds::sub::Sample<JoystickData::Movement> &m) {
+                    this->updateCameraCallback(float(m.data().panSpeed()), float(m.data().tiltSpeed()), float(m.data().zoomSpeed()));
+                  });
+  }
 };
--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -5,27 +5,18 @@
  *
  */
 
+#include "DeviceBinding.nsmap"
 #include <iostream>
-#include "stdio.h"
 #include "wsseapi.h"
 #include "wsaapi.h"
-#include <openssl/rsa.h>
-#include "DeviceBinding.nsmap"
-#include "soapDeviceBindingProxy.h"
-#include "soapMediaBindingProxy.h"
-#include "soapPTZBindingProxy.h"
-
-#include "soapPullPointSubscriptionBindingProxy.h"
-#include "soapRemoteDiscoveryBindingProxy.h"
-
 #include <stdarg.h> // For va_start, etc.
-#include <memory>   // For std::unique_ptr
-#include <ctime>
-#include <sstream> // For stringstream
 #include <algorithm>
-
-#define MAX_HOSTNAME_LEN 128
-#define MAX_LOGMSG_LEN 256
+#include <chrono>
+#include <thread> // std::thread, std::this_thread::sleep_for
+#include <src/onvif/OnvifDevice.hpp>
+#include <src/onvif/OnvifMedia.hpp>
+#include <src/onvif/OnvifPTZ.hpp>
+#include <src/cameraDDS.hpp>
 
 char *getCmdOption(char **begin, char **end, const std::string &option)
 {
@@ -42,37 +33,18 @@ bool cmdOptionExists(char **begin, char **end, const std::string &option)
 	return std::find(begin, end, option) != end;
 }
 
-void PrintErr(struct soap *_psoap)
-{
-	fflush(stdout);
-	// processEventLog(__FILE__, __LINE__, stdout, "error:%d faultstring:%s faultcode:%s faultsubcode:%s faultdetail:%s", _psoap->error,
-	std::cout << _psoap->error;
-	// *soap_faultstring(_psoap), *soap_faultcode(_psoap),*soap_faultsubcode(_psoap), *soap_faultdetail(_psoap));
-}
-
 int main(int argc, char *argv[])
 {
-
-	char szHostName[MAX_HOSTNAME_LEN] = {0};
 
 	// Proxy declarations
 	DeviceBindingProxy proxyDevice;
 	PTZBindingProxy proxyPTZ;
-	RemoteDiscoveryBindingProxy proxyDiscovery;
+	// RemoteDiscoveryBindingProxy proxyDiscovery;
 	MediaBindingProxy proxyMedia;
 
-	if (!(cmdOptionExists(argv, argv + argc, "-cIp") && cmdOptionExists(argv, argv + argc, "-cUsr") && cmdOptionExists(argv, argv + argc, "-cPwd")
-#ifdef WITH_FTP_UPLOAD
-		  && cmdOptionExists(argv, argv + argc, "-fIp") && cmdOptionExists(argv, argv + argc, "-fUsr") && cmdOptionExists(argv, argv + argc, "-fPwd")
-#endif
-			  ))
+	if (!(cmdOptionExists(argv, argv + argc, "-cIp") && cmdOptionExists(argv, argv + argc, "-cUsr") && cmdOptionExists(argv, argv + argc, "-cPwd")))
 	{
 		std::cout << "usage: ./ipconvif -cIp [<camera-ip>:<port>] -cUsr <cam-id> -cPwd <cam-pwd>\n";
-
-#ifdef WITH_FTP_UPLOAD
-		std::cout << "                  -fIp [<ftp-server-ip>:<port>] -fUsr <ftp-id> -fPwd <ftp-pwd>\n";
-#endif
-
 		return -1;
 	}
 
@@ -80,169 +52,55 @@ int main(int argc, char *argv[])
 	char *camUsr = getCmdOption(argv, argv + argc, "-cUsr");
 	char *camPwd = getCmdOption(argv, argv + argc, "-cPwd");
 
-	// #ifdef WITH_FTP_UPLOAD
-	//     char *ftpIp = getCmdOption(argv, argv+argc, "-fIp");
-	//     char *ftpUsr = getCmdOption(argv, argv+argc, "-fUsr");
-	//     char *ftpPwd = getCmdOption(argv, argv+argc, "-fPwd");
-	// #endif
+	std::string ptzConfigurationToken = "0";
+	std::string profile = "PTZ_ONVIF1";
+	std::string profileToken = "PTZ_ONVIF_TOKEN1";
 
-	//     if (!(camIp && camUsr && camPwd
-	// #ifdef WITH_FTP_UPLOAD
-	//     	   && ftpIp && ftpUsr && ftpPwd
-	// #endif
-	//         ))
-	//     {
-	//     	processEventLog(__FILE__, __LINE__, stdout, "Error: Invalid args (All args are required!)");
-	//     	return -1;
-	//     }
+	OnvifDevice device(camUsr, camPwd, camIp);
+	OnvifMedia media(device);
 
-	strcat(szHostName, "http://");
-	strcat(szHostName, camIp);
-	strcat(szHostName, "/onvif/device_service");
-
-	proxyDevice.soap_endpoint = szHostName;
-
-	// Register plugins
-	soap_register_plugin(proxyDevice.soap, soap_wsse);
-	soap_register_plugin(proxyDiscovery.soap, soap_wsse);
-	soap_register_plugin(proxyMedia.soap, soap_wsse);
-	soap_register_plugin(proxyPTZ.soap, soap_wsse);
-	struct soap *soap = soap_new();
-	// For DeviceBindingProxy
-	if (SOAP_OK != soap_wsse_add_UsernameTokenDigest(proxyDevice.soap, NULL, camUsr, camPwd))
+	_trt__GetProfileResponse getProfileResponse;
+	if (SOAP_OK == media.getProfile(profileToken, getProfileResponse))
 	{
-		return -1;
-	}
-
-	if (SOAP_OK != soap_wsse_add_Timestamp(proxyDevice.soap, "Time", 10))
-	{
-		return -1;
-	}
-
-	// Get Device info
-	_tds__GetDeviceInformation GetDeviceInformation;
-	_tds__GetDeviceInformationResponse GetDeviceInformationResponse;
-
-	if (SOAP_OK == proxyDevice.GetDeviceInformation(&GetDeviceInformation, GetDeviceInformationResponse))
-	{
-		std::cout << "Yay connection";
-		// processEventLog(__FILE__, __LINE__, stdout, "-------------------DeviceInformation-------------------");
-		// processEventLog(__FILE__, __LINE__, stdout, "Manufacturer:%sModel:%s\r\nFirmwareVersion:%s\r\nSerialNumber:%s\r\nHardwareId:%s", tds__GetDeviceInformationResponse->Manufacturer.c_str(),
-		//                 tds__GetDeviceInformationResponse->Model.c_str(), tds__GetDeviceInformationResponse->FirmwareVersion.c_str(),
-		//                 tds__GetDeviceInformationResponse->SerialNumber.c_str(), tds__GetDeviceInformationResponse->HardwareId.c_str());
+		std::cout << "retrieved profile\n";
 	}
 	else
 	{
-		std::cout << "Boo no connection\n";
-		PrintErr(proxyDevice.soap);
-		return -1;
+		std::cerr << "error happened retrieving profile\nCreating new one\n";
+
+		_trt__CreateProfileResponse createProfileResponse;
+		if (SOAP_OK == media.createProfile(profile, profileToken, createProfileResponse))
+		{
+			_trt__AddPTZConfigurationResponse addPTZConfigurationResponse;
+			if (SOAP_OK == media.addPtzConfiguration(profileToken, ptzConfigurationToken, addPTZConfigurationResponse))
+			{
+			}
+			else
+			{
+				std::cout << "Failed to create ptz configuration\n";
+			}
+		}
+		else
+		{
+			std::cout << "Failed to create profile\n";
+			return -1;
+		}
 	}
 
-	// TODO: Create ptz profile
-	// TODO: Test pan tilt
+	OnvifPTZ ptz(device, profileToken, ptzConfigurationToken);
 
-	//     // DeviceBindingProxy ends
-	//     soap_destroy(soap);
-	//     soap_end(soap);
+	CameraDDS dds;
 
-	//     // Get Device capabilities
-	// 	_tds__GetCapabilities *tds__GetCapabilities = soap_new__tds__GetCapabilities(soap, -1);
-	// 	tds__GetCapabilities->Category.push_back(tt__CapabilityCategory__All);
-	// 	_tds__GetCapabilitiesResponse *tds__GetCapabilitiesResponse = soap_new__tds__GetCapabilitiesResponse(soap, -1);
+	dds.setCameraUpdateCallback([&](float pan, float tilt, float zoom){
+		_tptz__ContinuousMoveResponse response;
+		ptz.continuousMove(pan, tilt, zoom, response);
+	});
 
-	// 	if (SOAP_OK == proxyDevice.GetCapabilities(tds__GetCapabilities, tds__GetCapabilitiesResponse))
-	// 	{
-
-	// 		if (tds__GetCapabilitiesResponse->Capabilities->Media != NULL)
-	// 		{
-	// 			processEventLog(__FILE__, __LINE__, stdout, "-------------------Media-------------------");
-	// 			processEventLog(__FILE__, __LINE__, stdout, "XAddr:%s", tds__GetCapabilitiesResponse->Capabilities->Media->XAddr.c_str());
-
-	// 			processEventLog(__FILE__, __LINE__, stdout, "-------------------streaming-------------------");
-	// 			processEventLog(__FILE__, __LINE__, stdout, "RTPMulticast:%s", (tds__GetCapabilitiesResponse->Capabilities->Media->StreamingCapabilities->RTPMulticast) ? "Y" : "N");
-	// 			processEventLog(__FILE__, __LINE__, stdout, "RTP_TCP:%s", (tds__GetCapabilitiesResponse->Capabilities->Media->StreamingCapabilities->RTP_USCORETCP) ? "Y" : "N");
-	// 			processEventLog(__FILE__, __LINE__, stdout, "RTP_RTSP_TCP:%s", (tds__GetCapabilitiesResponse->Capabilities->Media->StreamingCapabilities->RTP_USCORERTSP_USCORETCP) ? "Y" : "N");
-
-	// 			proxyMedia.soap_endpoint = tds__GetCapabilitiesResponse->Capabilities->Media->XAddr.c_str();
-
-	// 			// TODO: Add check to see if device is capable of 'GetSnapshotUri', if not skip this device!
-	// 		}
-	// 	}
-
-	// 	// For MediaBindingProxy
-	//     if (SOAP_OK != soap_wsse_add_UsernameTokenDigest(proxyMedia.soap, NULL, camUsr, camPwd))
-	// 	{
-	// 		return -1;
-	// 	}
-
-	// 	if (SOAP_OK != soap_wsse_add_Timestamp(proxyMedia.soap, "Time", 10))
-	// 	{
-	// 		return -1;
-	// 	}
-
-	// 	// Get Device Profiles
-	// 	_trt__GetProfiles *trt__GetProfiles = soap_new__trt__GetProfiles(soap, -1);
-	// 	_trt__GetProfilesResponse *trt__GetProfilesResponse = soap_new__trt__GetProfilesResponse(soap, -1);
-
-	// 	if (SOAP_OK == proxyMedia.GetProfiles(trt__GetProfiles, trt__GetProfilesResponse))
-	// 	{
-	//         _trt__GetSnapshotUriResponse *trt__GetSnapshotUriResponse = soap_new__trt__GetSnapshotUriResponse(soap, -1);
-	//         _trt__GetSnapshotUri *trt__GetSnapshotUri = soap_new__trt__GetSnapshotUri(soap, -1);
-
-	//         // processEventLog(__FILE__, __LINE__, stdout, "-------------------ONE SNAPSHOT PER PROFILE-------------------");
-
-	//         // Loop for every profile
-	//         for (int i = 0; i < trt__GetProfilesResponse->Profiles.size(); i++)
-	//         {
-	//             trt__GetSnapshotUri->ProfileToken = trt__GetProfilesResponse->Profiles[i]->token;
-
-	//             if (SOAP_OK != soap_wsse_add_UsernameTokenDigest(proxyMedia.soap, NULL, camUsr, camPwd))
-	//             {
-	//                 return -1;
-	//             }
-
-	//             // Get Snapshot URI for profile
-	//             if (SOAP_OK == proxyMedia.GetSnapshotUri(trt__GetSnapshotUri, trt__GetSnapshotUriResponse))
-	//             {
-	//                 processEventLog(__FILE__, __LINE__, stdout, "Profile Name- %s", trt__GetProfilesResponse->Profiles[i]->Name.c_str());
-	//                 processEventLog(__FILE__, __LINE__, stdout, "SNAPSHOT URI- %s", trt__GetSnapshotUriResponse->MediaUri->Uri.c_str());
-
-	//                 // Create a 'Snapshot' instance
-	//                 Snapshot *snapshot = new Snapshot("./downloads", "1.jpg");
-
-	//                 // Download snapshot file locally
-	//                 CURLcode result = snapshot->download(trt__GetSnapshotUriResponse->MediaUri->Uri);
-
-	// #ifdef WITH_FTP_UPLOAD
-	//                 if(CURLE_OK == result) {
-	//                     std::string upload = snapshot->getUploadUri(gateName, ftpIp, ftpUsr, ftpPwd);
-	//                     processEventLog(__FILE__, __LINE__, stdout, "Uploading using: %s", upload.c_str());
-	//                     system(upload.c_str());
-	//                     sleep(5);
-	//                 }
-	// #endif
-
-	//                 // Delete current 'Snapshot' instance
-	//                 // delete snapshot;
-
-	//                 // Break out of the media profiles loop (now that we have one snapshot uploaded),
-	//                 // so we only take one Snapshot per camera.
-	//                 break;
-	//             }
-	//             else
-	//             {
-	//                 PrintErr(proxyMedia.soap);
-	//             }
-	//         }
-	// 	}
-	// 	else
-	// 	{
-	// 		PrintErr(proxyMedia.soap);
-	// 	}
-
-	// MediaBindingProxy ends
-	soap_destroy(soap);
-	soap_end(soap);
+	while (true)
+	{
+		dds.update();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
 
 	return 0;
 }
